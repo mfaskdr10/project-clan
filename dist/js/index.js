6a5c122162b66c771602de75fc9b7f8dc573c399
mfaskdr10 <mfaskdr10@gmail.com>
2024-08-28 20:59:06 +0700

    menambahkan codingan js

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,12 @@
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
 
-navToggle.addEventListener('click', function() {
-  navMenu.classList.toggle('active');
-})
+navToggle.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", function (e) {
+  if (!navToggle.contains(e.target)) {
+    navMenu.classList.remove("active");
+  }
+});
